06d81fbe379a3ba68f3a495de56481b04bb08f45
Rachel Connolly <rshconn@gmail.com>
2019-10-14 18:12:24 +0000

    find_first_available_student, find_first_available_company

--- match.py
+++ match.py
@@ -10,42 +10,58 @@ class Company:
         """
         self.name = name
         self.max_matches = max_matches
-        self.ranked_students = ranked_students  #
+        self.ranked_students = ranked_students
         self.matches = []
 
 class Student:
-    def __init__(self, first_name, last_name, unique_id, ranked_companies):
+    def __init__(self, name, unique_id, ranked_companies):
         """
         Parameters
         ----------
-        first_name : str
-        last_name : str
-        unique_id : int
+        name : str
+        unique_id : str
         ranked_companies : list of str
             In order list of the names of ranked companies 
         """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.id = id
+        self.name = name
+        self.unique_id = unique_id
         self.ranked_companies = ranked_companies
-        self.match = None
 
 
-def find_first_available(rankings, available):
+def find_first_available_student(ranked_students, available_students):
     """
     Parameters
     ---------
-    rankings : list of str
-        In order list o
-    available : list of str
+    ranked_students : list of str
+    available_students : list of Student
 
     Returns
     ------
+    Student or None
 
     """
-    for item in rankings:
-        if item in available:
-            return item
+    for student_id in ranked_students:
+        student = next((student for student in available_students if student.unique_id == student_id), None)
+        if student:
+            return student
+    return None
+
+
+def find_first_available_company(ranked_companies, available_companies):
+    """
+    Parameters
+    ---------
+    ranked_companies : list of str
+    available_companies : list of Company
+
+    Returns
+    ------
+    Company or None
+    """
+    for company_name in ranked_companies:
+         company = next((company for company in available_companies if company.name == company_name), None)
+         if company: 
+             return company
     return None
 
 
@@ -63,18 +79,17 @@ def mutual_match(companies, students):
     matches = {}
     unmatched_companies = []
     unmatched_students = []
-    import sys; print(sys.version)
     while len(companies) > 0:
         available_companies = []
         
         for company in companies:
-            student = find_first_available(company.ranked_students, students)
+            student = find_first_available_student(company.ranked_students, students)
             
             if student:
-                company_choice = find_first_available(student.ranked_companies, [company.name for company in companies])
-            
+                company_choice = find_first_available_company(student.ranked_companies, companies)
+
                 if company_choice:
-                    if company.name == company_choice:
+                    if company.name == company_choice.name:
 
                         # Assign student to company
                         matches[student] = company.name
@@ -85,13 +100,13 @@ def mutual_match(companies, students):
 
                         if len(company.matches) < company.max_matches:
                             available_companies.append(company)
-                    else:
+                    else:   # Student's first available choice is not company
                             available_companies.append(company)
-                else:
+                else:   # None of student's ranked companies remain
                     print(f"Cannot match {student}")
                     unmatched_students.append(student)
                     available_companies.append(company)
-            else:
+            else:   # None of company's ranked students remain
                 print(f'Cannot match {company.name}')
                 unmatched_companies.append(company)
 
@@ -100,12 +115,13 @@ def mutual_match(companies, students):
     return matches, unmatched_companies, unmatched_students
 
 
-def unilateral_match(unmatched_students, unmatched_companies):
-    """
+def unilateral_match(unmatched_companies, unmatched_students):
+    """Match unmatched students with companies that have ranked those students
+    
     Parameters
     ----------
+    unmatched_companies : list of Company
     unmatched_students : list of Student
-    unmatched_companies : list of Company 
 
     Returns
     -------
@@ -118,10 +134,12 @@ def unilateral_match(unmatched_students, unmatched_companies):
                 if student in unmatched_students:
                     matches[student] = company.name
                     company.matches.append(student)
+                    unmatched_students.remove(student)
+                    unmatched_companies.remove(company)
                     break
-            print()
+            print(f'No unmatched students available for {company.name}')
         else: 
-            print(f'{company.name} ')
+            print(f'{company.name} has {len(company.matches)}')
     
     return matches, unmatched_companies, unmatched_students
 
--- test_match.py
+++ test_match.py
@@ -1,72 +1,128 @@
-from match import Company, Student, mutual_match, unilateral_match
+from match import (Company, Student, find_first_available_company, find_first_available_student,
+                   mutual_match, unilateral_match)
+
+def test_find_first_available_company():
+    ranked_companies = ['A', 'B', 'C']
+    company_to_find = Company('C', 1, ['0', '1'])
+    available_companies = [company_to_find]
+    company = find_first_available_company(ranked_companies, available_companies)
+    assert company == company_to_find
+    
+    
+def test_find_first_available_student():
+    ranked_students = ['000', '111']
+    student_to_find = Student('111', '111', ['B', 'A'])
+    available_students = [
+        Student('222', '222', ['A', 'B']),
+        student_to_find
+    ]
+    student = find_first_available_student(ranked_students, available_students)
+    assert student ==  student_to_find
+
 
 def test_mutual_match():
-    companies = {
+    companies = [
         Company('A', 1, ['0', '1']),
         Company('B', 1, ['1', '0'])
-    }
-    
-    students = [
-        Student('0', '0', 0, ['A', 'B']),
-        Student('1', '1', 1, ['B', 'A'])
     ]
     
-    matches = mutual_match(companies, students)
+    students = [
+        Student('0', '0', ['A', 'B']),
+        Student('1', '1', ['B', 'A'])
+    ]
+    
+    matches, unmatched_companies, unmatched_students = mutual_match(companies, students)
 
     assert matches == {'0': 'A', '1': 'B'}
-    print('passed test_simple_match')
+    assert unmatched_companies == []
+    assert unmatched_students == []
 
 def test_mutual_match_two_rounds():
-    companies = {
+    companies = [
         Company('A', 1, ['0', '1']),
         Company('B', 1, ['0', '1'])
-    }
+    ]
     
     students = [
-        Student('0', '0', 0, ['A', 'B']),
-        Student('1', '1', 1, ['B', 'A'])
+        Student('0', '0', ['A', 'B']),
+        Student('1', '1', ['B', 'A'])
     ]
 
     matches = mutual_match(companies, students)
     assert matches == {'0': 'A', '1': 'B'}
-    print('passed test_match_two_rounds')
 
 def test_no_mutual_match():
-    companies = {
+    companies = [
         Company('A', 1, ['0', '1']),
         Company('B', 1, ['0', '1']),
         Company('C', 1, ['0', '1'])
-    }
+    ]
     
     students = [
-        Student('0', '0', 0, ['A', 'B']),
-        Student('1', '1', 1, ['B', 'A'])
+        Student('0', '0', ['A', 'B']),
+        Student('1', '1', ['B', 'A'])
     ]
     
     matches = mutual_match(companies, students)
     assert matches == {'0': 'A', '1': 'B'}
-    print('passed test_no_match')
 
 def test_more_than_one_mutual_match():
-    companies = {
+    companies = [
         Company('A', 2, ['0', '1', '2', '3']),
         Company('B', 1, ['0', '1']),
-    }
+    ]
     
     students = [
-        Student('0', '0', 0, ['A', 'B']),
-        Student('1', '1', 1, ['B', 'A']),
-        Student('2', '2', 2, ['A']),
-        Student('3', '3', 3, ['A'])
+        Student('0', '0', ['A', 'B']),
+        Student('1', '1', ['B', 'A']),
+        Student('2', '2', ['A']),
+        Student('3', '3', ['A'])
     ]
 
-    
     matches = mutual_match(companies, students)
     assert matches == {'0': 'A', '1': 'B', '2': 'A'}
-    print('passed test_more_than_one_match')
 
-if __name__ == "__main__":
-	test_mutual_match()
-	test_mutual_match_two_rounds()
-	test_no_mutual_match()
-	test_more_than_one_mutual_match()
+def test_unilateral_match():
+    unmatched_companies = [
+        Company('AA', 1, ['0', '1']),
+    ]
+    
+    unmatched_students = [
+        Student('0', '0', ['A', 'B']),
+        Student('1', '1', ['B', 'A'])
+    ]
+    
+    matches, unmatched_companies, unmatched_students = unilateral_match(
+        unmatched_companies, unmatched_students)
+        
+    assert matches == {}
+    assert unmatched_companies == []
+    assert unmatched_students == []
+
+
+def test_unilateral_match_unmatched_company():
+    unmatched_companies = [
+        Company('AA', 1, ['0', '1']),
+    ]
+    
+    unmatched_students = []
+    
+    matches, unmatched_companies, unmatched_students = unilateral_match(
+        unmatched_companies, unmatched_students)
+        
+    assert matches == {}
+    assert unmatched_companies == [ Company('AA', 1, ['0', '1'])]
+    assert unmatched_students == []
+
+
+def test_unilateral_match_unmatched_student():
+    unmatched_companies = []
+    
+    unmatched_students = [Student('0', '0', ['A', 'B'])]
+    
+    matches, unmatched_companies, unmatched_students = unilateral_match(
+        unmatched_companies, unmatched_students)
+        
+    assert matches == {}
+    assert unmatched_companies == []
+    assert unmatched_students == [Student('0', '0', ['A', 'B'])]
